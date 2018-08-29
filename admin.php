e71b2153421294fee88eb9f9fd740f6569bf7f73
gr0spy <shamilkerimli@gmail.com>
2018-08-29 13:31:41 +0400

    Changes
    Changes

--- admin.php
+++ admin.php
@@ -1,10 +1,5 @@
 <?php
-// Copyright (C) 2010 Rod Roark <rod@sunsetsystems.com>
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+
 
 // Checks if the server's PHP version is compatible with OpenEMR:
 require_once(dirname(__FILE__) . "/common/compatibility/Checker.php");
