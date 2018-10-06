5030e9b74ad63cbde21931a047b66981694ced10
gr0spy <shamilkerimli@gmail.com>
2018-10-06 12:54:32 +0400

    Changes to the file upload structure
    Changes to the file upload structure

--- interface/patient_file/summary/vitals_fragment.php
+++ interface/patient_file/summary/vitals_fragment.php
@@ -40,13 +40,23 @@ require_once("../../globals.php");
     </style>
   </head>
   <body>
-    <p>Burada göstərilən pasientin kardiqramıdır. (Real Time)</p>
+
+    <p> .csv datanı buradan yüklə </p>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+
+    <form action="SecondUpload.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="my-file" size="50" maxlength="25"> <br> 
+  <input type="submit" name="upload" value="Upload">
+</form>
+
+    <h4>Burada göstərilən pasientin kardiqramıdır. (Real Time)</h4>
     <p>
       No roll period.
     </p>
     <div id="noroll" style="width:800px; height:320px;"></div>
 
     
+  
 
     <script type="text/javascript" language= ”JavaScript”>
       g1 = new Dygraph(
@@ -58,6 +68,9 @@ require_once("../../globals.php");
       );
       
     </script>
+
+        
+
 
 
        <h2> Istifadəçi fayyları ilə əməliyatlar şöbəsi : </h2> 
